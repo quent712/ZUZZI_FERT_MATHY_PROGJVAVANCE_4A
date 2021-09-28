1dc11193f2986f4d829ef40eb7052964f7beb85a
quent <quentin2000.fert@gmail.com>
2021-09-28 14:18:11 +0200

    some changes mcts1

--- Assets/Scripts/agent/MCTS1.cs
+++ Assets/Scripts/agent/MCTS1.cs
@@ -3,26 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MCTS1 
+public class MCTS1
 {
     private Node tree;
-    public static float FREQUENCY = 0.4f;  // Fréquence des actions du MCTS
+    public static float FREQUENCY = 0.4f; // Fréquence des actions du MCTS
     private int[] a; // matrice des touches directionnelles
     private float born;
-    
+    private CharacterRender render;
 
-    public MCTS1() : base(){
-        tree = new Node(new Register(0,0));
+
+    public MCTS1() : base()
+    {
+        tree = new Node(new Register(0, 0));
         born = 0.0f;
         a = new int[4];
     }
 
-    public bool thrust(){
-        foreach(Node n in tree.getPossibleAction()){
-            if(n.data.b > 20){ // au moins un des noeuds doit être fiable (>20)
+    public bool thrust()
+    {
+        foreach (Node n in tree.getPossibleAction())
+        {
+            if (n.data.b > 20)
+            {
+                // au moins un des noeuds doit être fiable (>20)
                 return true;
             }
         }
+
         return false;
     }
 
@@ -40,9 +47,9 @@ public class MCTS1
             compute(tree); //compute(tree,pokemonMe, pokemonAdv);
         }
 
-       
-          // Appel horloge
-        if(thrust())
+
+        // Appel horloge
+        if (thrust())
         {
             born = FREQUENCY;
 
@@ -51,18 +58,24 @@ public class MCTS1
             Node n = null;
             bool priorityMove = false;
             // Si une bombe est proche de l'objet on gère une priorité
-            foreach(GameObject o in GameObject.FindGameObjectsWithTag("bomb")){
-                if(Vector2.Distance(o.transform.position, render.transform.position) < 2f){
-                    priorityMove = true;;
+            foreach (GameObject o in GameObject.FindGameObjectsWithTag("bomb"))
+            {
+                if (Vector2.Distance(o.transform.position, render.transform.position) < 2f)
+                {
+                    priorityMove = true;
+                    ;
                 }
             }
 
             // Cherche la meilleure action conduisant à une victoire
-            foreach(Node child in tree.getPossibleAction()){
-                if(child.state != PossibleAction.UNDETERMINED){
-                    if((float)child.data.a / (float)child.data.b > max ){
+            foreach (Node child in tree.getPossibleAction())
+            {
+                if (child.state != PossibleAction.UNDETERMINED)
+                {
+                    if ((float) child.data.a / (float) child.data.b > max)
+                    {
                         currentAction = child.state;
-                        max = (float)child.data.a / (float)child.data.b;
+                        max = (float) child.data.a / (float) child.data.b;
                         n = child;
                     }
                 }
@@ -71,13 +84,14 @@ public class MCTS1
             // Si priorité de mouvement ou action ESQUIVE
             // On se déplace
             a = new int[4];
-            if(priorityMove || currentAction == PossibleAction.ESQUIVE){
-                if(render.v.x >= 0)
+            if (priorityMove || currentAction == PossibleAction.WALK)
+            {
+                if (render.v.x >= 0)
                     a[2] = 1;
                 else
                     a[3] = 1;
-                
-                if(render.v.y >= 0)
+
+                if (render.v.y >= 0)
                     a[1] = 1;
                 else
                     a[0] = 1;
@@ -85,10 +99,11 @@ public class MCTS1
 
 
             int i = 0;
-            if(characMe != null
-               && characAdv != null)
+            if (characMe != null
+                && characAdv != null)
+            {
+                switch (currentAction)
                 {
-                switch(currentAction){
                     case PossibleAction.UNDETERMINED:
                     case PossibleAction.WAIT:
                         break;
@@ -108,74 +123,92 @@ public class MCTS1
                         break;
                 }
             }
+
             // IMPORTANT ! On définie le nouveau noeud de base sur le noeud choisi
-            if(n != null)
+            if (n != null)
                 tree = n;
 
             return i;
         }
 
-    // Applique le mouvement
-    if( characMe != null){
-                if(a[0] >= 0.8f){
-                    render.v.y += Time.deltaTime * SENSIBILITY;
-                    pokemonMe.charge(COST_MOVE);
-                }
-                if(a[1] >= 0.8f){
-                    render.v.y -= Time.deltaTime * SENSIBILITY;
-                    pokemonMe.charge(COST_MOVE);
-                }
-                if(a[2] >= 0.8f){
-                    render.v.x -= Time.deltaTime * SENSIBILITY;
-                    pokemonMe.charge(COST_MOVE);
-                }
-                if(a[3] >= 0.8f){
-                    render.v.x += Time.deltaTime * SENSIBILITY;
-                    pokemonMe.charge(COST_MOVE);
-                }
+        // Applique le mouvement
+        if (characMe != null)
+        {
+            if (a[0] >= 0.8f)
+            {
+                render.v.y += Time.deltaTime * SENSIBILITY;
+                pokemonMe.charge(COST_MOVE);
             }
-            return 0;
+
+            if (a[1] >= 0.8f)
+            {
+                render.v.y -= Time.deltaTime * SENSIBILITY;
+                pokemonMe.charge(COST_MOVE);
+            }
+
+            if (a[2] >= 0.8f)
+            {
+                render.v.x -= Time.deltaTime * SENSIBILITY;
+                pokemonMe.charge(COST_MOVE);
+            }
+
+            if (a[3] >= 0.8f)
+            {
+                render.v.x += Time.deltaTime * SENSIBILITY;
+                pokemonMe.charge(COST_MOVE);
+            }
+        }
+
+        return 0;
     }
-    
-    
-    
-    
-    void compute(Node action){
+
+
+
+
+    void compute(Node action)
+    {
         //Debug.Log(action.data.a + "/" + action.data.b);
         //Tant que la simulation n'est pas achevée
-        while(!GameSimul.isFinished){
+        while (!GameSimul.isFinished)
+        {
             System.Array actions = GameSimul.GetNextPossibleAction(action);
 
             // Choisi une action au piff
-            PossibleAction choice = (PossibleAction)GameSimul.GetRandomAction(actions);
-            
+            PossibleAction choice = (PossibleAction) GameSimul.GetRandomAction(actions);
+
             // Crée un node (donc une action) si elle n'existe pas encore
             // ou sinon prend celle trouvée
             Node exitanteNode = action.Exist(choice);
-            if(exitanteNode == null){
-                Node selectedAction = action.AddChild(new Register(0,0));
+            if (exitanteNode == null)
+            {
+                Node selectedAction = action.AddChild(new Register(0, 0));
                 selectedAction.parent = action;
                 selectedAction.setState(choice);
-                
+
                 action = selectedAction;
-            }else{
+            }
+            else
+            {
                 action = exitanteNode;
             }
+
             // Lance la simulation 
             GameSimul.PlayAction(action);
             //Debug.Log(GameSimul.lifeAdv + " | " + GameSimul.lifeMe);
             //if(i++ > 10000) break;
         }
+
         // Applique des valeurs sur la feuille finale
         action.data.b = 1;
-        if(GameSimul.finalSituation == 0)//gameover
+        if (GameSimul.finalSituation == 0) //gameover
             action.data.a = 0;
-        else                    //win
+        else //win
             action.data.a = 1;
 
         // Retroprograpagation de l'action
         Node.Retropropagation(action);
         // Prépare le simulateur à une prochaine simulation
         GameSimul.Reset();
-    
+
+    }
 }
